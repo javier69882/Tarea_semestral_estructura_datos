858d8dfbb0961c0f9d1fa0e12f7b54c89b0a6685
Javier <javier69882@gmail.com>
2026-06-13 17:41:06 -0400

    se añadio pesos en el adt de grafo

--- Graph.cpp
+++ Graph.cpp
@@ -11,37 +11,48 @@ Graph::Graph(bool directed) : isDirected(directed){
 GraphList::GraphList(bool directed) : Graph(directed) {}
 
 void GraphList::addVertex() { 
-    adjList.push_back(std::vector<int>()); 
+    adjList.push_back(std::vector<std::pair<int, double>>());
 }
 
-void GraphList::addEdge(int u, int v) {
+void GraphList::addEdge(int u, int v, double weight){
     if (u < 0 || u >= adjList.size() || v < 0 || v >= adjList.size()) {
         throw std::out_of_range("Vértice no creado");
     }
 
     // busco duplicados antes de insertar
     //necesario, porque en el dataset de proteinas, que es no dirigido, hay aristas que podrian quedar repetidas, error que con matriz de adyacencia no ocurre
-    for (int vecino : adjList[u]) {
-        if (vecino == v) {
-            return; // si ya son vecinos, salimos inmediatamente y no insertamos nada
+    for (const auto& arista : adjList[u]) {
+        if (arista.first == v) {
+            return; // Si ya son vecinos, salimos
         }
     }
 
-    //si el bucle termina y no se activo el return, significa que la arista es nueva
-    adjList[u].push_back(v);
+    //si el bucle termina y no se activo el return, significa que la arista es nueva, inserta par, vertice peso
+    adjList[u].push_back({v, weight});
     
-    if (!isDirected && u != v) {
-        adjList[v].push_back(u);
+   if (!isDirected && u != v) {
+        adjList[v].push_back({u, weight});
     }
 }
 
 void GraphList::removeEdge(int u, int v) {
     if (u < 0 || u >= adjList.size() || v < 0 || v >= adjList.size()) return;
 
-    adjList[u].erase(std::remove(adjList[u].begin(), adjList[u].end(), v), adjList[u].end());
+    // Borrado manual buscando el destino 
+    for (auto it = adjList[u].begin(); it != adjList[u].end(); ++it) {
+        if (it->first == v) {
+            adjList[u].erase(it);
+            break;
+        }
+    }
 
     if (!isDirected && u != v) {
-        adjList[v].erase(std::remove(adjList[v].begin(), adjList[v].end(), u), adjList[v].end());
+        for (auto it = adjList[v].begin(); it != adjList[v].end(); ++it) {
+            if (it->first == u) {
+                adjList[v].erase(it);
+                break;
+            }
+        }
     }
 }
 
@@ -51,15 +62,32 @@ int GraphList::getNumVertices() const {
 
 std::vector<int> GraphList::getNeighbors(int vertex) const {
     if (vertex < 0 || vertex >= adjList.size()) throw std::out_of_range("Vértice no creado");
-    return adjList[vertex];
+    
+    
+    std::vector<int> vecinos;
+    for (const auto& arista : adjList[vertex]) {
+        vecinos.push_back(arista.first);
+    }
+    return vecinos;
+}
+
+double GraphList::getWeight(int u, int v) const {
+    if (u < 0 || u >= adjList.size() || v < 0 || v >= adjList.size()) return 0.0;
+    
+    for (const auto& arista : adjList[u]) {
+        if (arista.first == v) {
+            return arista.second; // Retornamos el peso
+        }
+    }
+    return 0.0; 
 }
 
 void GraphList::print() const {
-    std::cout << "Lista de adyacencia:\n";
+    std::cout << "Lista de adyacencia (con pesos):\n";
     for (size_t i = 0; i < adjList.size(); i++) {
         std::cout << i << ":";
-        for (int neighbor : adjList[i]) {
-            std::cout << " -> " << neighbor;
+        for (const auto& arista : adjList[i]) {
+            std::cout << " -> " << arista.first << " [W:" << arista.second << "]";
         }
         std::cout << "\n";
     }
@@ -74,33 +102,40 @@ void GraphList::print() const {
 GraphMatrix::GraphMatrix(bool directed) : Graph(directed), n(0) {}
 
 GraphMatrix::GraphMatrix(int V, bool directed) : Graph(directed), n(V) { 
-    adjMatrix.resize(n, std::vector<int>(n, 0)); 
+    //inicializamos la matriz con 0.0 (double)
+    adjMatrix.resize(n, std::vector<double>(n, 0.0)); 
 }
 
 void GraphMatrix::addVertex() {
     n++;
     for (auto &row : adjMatrix) {
-        row.push_back(0);
+        row.push_back(0.0);
     }
-    adjMatrix.push_back(std::vector<int>(n, 0));
+    adjMatrix.push_back(std::vector<double>(n, 0.0));
 }
 
-void GraphMatrix::addEdge(int u, int v) {
+void GraphMatrix::addEdge(int u, int v, double weight) {
     if (u < 0 || u >= n || v < 0 || v >= n) throw std::out_of_range("Vértice no creado");
 
-    adjMatrix[u][v] = 1;
+    adjMatrix[u][v] = weight; // Guardamos el peso directamente
     if (!isDirected && u != v) {
-        adjMatrix[v][u] = 1;
+        adjMatrix[v][u] = weight;
     }
 }
 
 void GraphMatrix::removeEdge(int u, int v) {
     if (u < 0 || u >= n || v < 0 || v >= n) return;
 
-    adjMatrix[u][v] = 0;
+    adjMatrix[u][v] = 0.0;
     if (!isDirected && u != v) {
-        adjMatrix[v][u] = 0;
+        adjMatrix[v][u] = 0.0;
     }
+}
+
+// 
+double GraphMatrix::getWeight(int u, int v) const {
+    if (u < 0 || u >= n || v < 0 || v >= n) return 0.0;
+    return adjMatrix[u][v];
 }
 
 int GraphMatrix::getNumVertices() const {
@@ -111,7 +146,7 @@ std::vector<int> GraphMatrix::getNeighbors(int vertex) const {
     if (vertex < 0 || vertex >= n) throw std::out_of_range("Vértice no creado");
     std::vector<int> neighbors;
     for (int i = 0; i < n; i++) {
-        if (adjMatrix[vertex][i] == 1) {
+        if (adjMatrix[vertex][i] != 0.0) { //Si el peso no es 0, hay conexion
             neighbors.push_back(i);
         }
     }
