309af5e91bab9cce13462a6c40c74405a73d62cb
Ariel <fernandezariel288@gmail.com>
2026-06-27 12:35:45 -0400

    Documentado de codigo

--- experimento.hpp
+++ experimento.hpp
@@ -11,10 +11,6 @@ private:
 public:
     // Función principal
     static void ejecutarPerturbacion(Graph& g);
-    
-    // Aquí pienso añadir ciertas funciones como:
-    // ejecuta las métricas 10 veces para sacar la varianza.
-    // static void ejecutarEstudioDeTiempos(Graph& g, int iteraciones = 10);
 };
 
 #endif // EXPERIMENTO_HPP
